cfe4a31a3eea502112727865b806fe34a9761e6b
Hung Ngo Tai <ngotaihung@gmail.com>
2020-09-05 19:54:46 +0700

    Guarding routes

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,7 +9,7 @@ const navigationItems = (props) => {
       <NavigationItem link="/" exact>Burger Builder</NavigationItem>
       {props.isAuth ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
       {
-        !props.isAuth ? <NavigationItem link="/auth">Auth</NavigationItem> : <NavigationItem link="/logout">Logout</NavigationItem>
+        !props.isAuth ? <NavigationItem link="/auth">Authenticate</NavigationItem> : <NavigationItem link="/logout">Logout</NavigationItem>
       }
     </ul>
   )
