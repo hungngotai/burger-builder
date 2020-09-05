19abebe9bca328e021eff23333ff9a15a8805848
Ngo Tai Hung <nt-hung@ruby-dev.jp>
2020-09-05 11:03:13 +0700

    Forwarding unauthenticated users

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,7 @@ const navigationItems = (props) => {
   return (
     <ul className={classes.NavigationItems}>
       <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-      <NavigationItem link="/orders">Orders</NavigationItem>
+      {props.isAuth ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
       {
         !props.isAuth ? <NavigationItem link="/auth">Auth</NavigationItem> : <NavigationItem link="/logout">Logout</NavigationItem>
       }
